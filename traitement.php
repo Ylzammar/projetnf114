84664360843b93e121d79007d408fb1c09bca6ec
Ylzammar <123626400+Ylzammar@users.noreply.github.com>
2023-03-22 14:17:39 +0100

    kkk

--- traitement.php
+++ traitement.php
@@ -1,4 +1,11 @@
 <?php
-$NAME = $_GET['nom'];
-echo 'Bonjour '.$NAME;
+    // Récupérer le mot entré par l'utilisateur
+    $mot = $_POST["mot"];
+
+    // Vérifier si le mot est un palindrome
+    if (strtolower($mot) == strtolower(strrev($mot))) {
+        echo "Le mot ".$mot." est un palindrome !";
+    } else {
+        echo "Le mot ".$mot." n'est pas un palindrome.";
+    }
 ?>
